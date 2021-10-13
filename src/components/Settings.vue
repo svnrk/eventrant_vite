f15bfd90a61846bf418173054e8ffc4f0e53d6b5
Ami Sild <amisild@gmail.com>
2021-10-13 22:59:27 +0300

    added little details in settings

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
-      <div class="container">
-
-        <h3>Settings </h3>
-      </div>
-
+    <div class="container">
+      <tekst>My profile</tekst>
+      <br />
+      <tekst>Get Premium</tekst>
+      <br />
+      <tekst>Help</tekst>
+      <br />
+      <tekst>Sign out</tekst>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   // name: AddEvent,
-
-}
+};
 </script>
 
 <style>
-
+tekst {
+  font-size: 20px;
+  font-weight: 500;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
 </style>
