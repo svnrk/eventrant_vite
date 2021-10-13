6785ac3dbcc7d5b048fa3eb033d1435eea0051ed
Ami Sild <amisild@gmail.com>
2021-10-13 18:12:08 +0300

    finished the page, but still missing few functions

--- src/components/AddEvent.vue
+++ src/components/AddEvent.vue
@@ -7,20 +7,23 @@
 
       <div class="borders">
         <Small_Text_header>Üritus:</Small_Text_header>
-        <p style="white-space: pre-line; color: black">{{ message }}</p>
-        <textarea v-model="message" placeholder="lisa üritus"></textarea>
+        <p style="white-space: pre-line">{{ message_event }}</p>
+        <textarea v-model="message_event" placeholder="lisa üritus"></textarea>
 
         <Small_Text_header>Esineja:</Small_Text_header>
-        <p style="white-space: pre-line">{{ message }}</p>
-        <textarea v-model="message" placeholder="lisa esineja(d)"></textarea>
+        <p style="white-space: pre-line">{{ message_performer }}</p>
+        <textarea
+          v-model="message_performer"
+          placeholder="lisa esineja(d)"
+        ></textarea>
 
         <Small_Text_header>Aeg:</Small_Text_header>
-        <!-- <p style="white-space: pre-line;">{{ message }}</p> -->
-        <textarea v-model="message" placeholder="lisa aeg"></textarea>
+        <p style="white-space: pre-line">{{ message_time }}</p>
+        <textarea v-model="message_time" placeholder="lisa aeg"></textarea>
 
         <Small_Text_header>Koht:</Small_Text_header>
-        <p style="white-space: pre-line">{{ message }}</p>
-        <textarea v-model="message" placeholder="lisa koht"></textarea>
+        <p style="white-space: pre-line">{{ message_place }}</p>
+        <textarea v-model="message_place" placeholder="lisa koht"></textarea>
         <br />
       </div>
 
@@ -43,12 +46,12 @@ export default {
       alert("Uus sündmus on lisatud!");
     },
   },
-  // https://bootstrap-vue.org/docs/components/form-textarea
 };
 </script>
 
 <style scoped>
 .borders {
+  color: black;
   border: 1px solid #2c3e50;
   margin: 5px auto;
   padding: 10px 20px;
@@ -69,26 +72,12 @@ export default {
 
 Pealkiri {
   font-size: 30px;
-  font-weight: 600;
+  font-weight: 500;
   align-items: center;
   display: flex;
   justify-content: center;
 }
-header {
-  color: rgb(255, 255, 255);
-  justify-content: space-between;
-  align-items: "position";
-  margin-bottom: 20px;
-}
-Text {
-  font-size: 20px;
-  font-weight: 500;
-  color: rgb(255, 255, 255);
-  display: flex;
-  justify-content: space-between;
-  align-items: "position";
-  margin-bottom: 15px;
-}
+
 Small_Text_header {
   font-size: 25px;
   color: rgb(255, 255, 255);
@@ -96,9 +85,5 @@ Small_Text_header {
   justify-content: space-between;
   align-items: "position";
   margin-bottom: 15px;
-}
-span {
-  float: right;
-  text-align: right;
 }
 </style>
